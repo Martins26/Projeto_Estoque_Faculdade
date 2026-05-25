ea0bb70bc9e523f74f4d6996e989e9b635529c56
Mário Eduardo <marioed2604@gmail.com>
2026-05-25 20:10:44 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -40,15 +40,17 @@ def registrar_saida(produto_id, quantidade):
 def consultar_estoque(produto_id):
     if produto_id in estoque:
         produto = estoque[produto_id]
+        texto = (
+            f"Nome: {produto['nome']}\n"
+            f"Categoria: {produto['categoria']}\n"
+            f"Preço: R$ {produto['preco']}\n"
+            f"Quantidade: {produto['quantidade']}"
+        )
 
-        print("\n=== DETALHES DO PRODUTO ===")
-        print(f"Nome:       {produto['nome']}")
-        print(f"Categoria:  {produto['categoria']}")
-        print(f"Preço:      R$ {produto['preco']}")
-        print(f"Quantidade: {produto['quantidade']} unidades")
-        print("===========================")
+        return texto
+
     else:
-        print("Produto não encontrado")
+        return "Produto não encontrado"
 
 
 def alertar_estoque_baixo(limite):
@@ -62,6 +64,9 @@ def alertar_estoque_baixo(limite):
                 f"{informacoes['quantidade']} unidades"
             )
 
+def mostrar_estoque():
+    texto_estoque["text"] = consultar_estoque(1)
+
 
 cadastrar_produtos(3, "Cenoura", "verdura", 3.50, 15)
 
@@ -73,6 +78,15 @@ consultar_estoque(1)
 
 alertar_estoque_baixo(20)
 
+
 janela = Tk()
+janela.title("Sistema de Controle de Estoque")
+
+texto_orientacao = Label(janela, text="Bem-vindo ao Sistema de Controle de Estoque!")
+texto_orientacao.grid(column=0, row=0)
+botao = Button(janela, text="Clique aqui para ver o estoque", command= mostrar_estoque)
+botao.grid(column=0, row=1)
+texto_estoque = Label(janela, text="")
+texto_estoque.grid(column=0, row=2)
 
 janela.mainloop()
